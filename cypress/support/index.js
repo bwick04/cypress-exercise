bc744c4e2a427a52924d63763500e2b27a516b7b
Benny Wick <benny.wick@crowdstreet.com>
2022-05-15 17:14:48 -0700

    touch up code

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,13 +14,3 @@
 // ***********************************************************
 
 import "./commands";
-
-// // /cypres/support/index.js
-// Cypress.on("window:before:load", (win) => {
-//   cy.stub(win.console, "error").callsFake((msg) => {
-//     // log out to the terminal
-//     cy.now("task", "error", msg);
-//     // log to Command Log and fail the test
-//     throw new Error(msg);
-//   });
-// });
