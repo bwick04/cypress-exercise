aaba646891821179fe2f601ac3bdb2505c96e358
Benny Wick <benny.wick@gmail.com>
2022-05-15 20:25:39 -0700

    Update navigate.js

--- cypress/integration/Navigate/navigate.js
+++ cypress/integration/Navigate/navigate.js
@@ -28,7 +28,8 @@ And("All linked pages are live", () => {
       hrefLink &&
       // Exclude the mailto link that launches system mail application
       !hrefLink.includes("mailto") &&
-      // Exclude the skip link that takes you to the bottom of the page since we're already verify the current page
+      // Exclude the link that takes you to the bottom of the page since we're already verifying the current page
+      // and this will cause the badpage test to fail
       !hrefLink.includes("#w3c_content_body") &&
       // There is currently a dead link in the copyright section of the footer which is causing the test to fail
       // for the purpose of this exercise and having the test cases pass, I have excluded that link in the test for now.
